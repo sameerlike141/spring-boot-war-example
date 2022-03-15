f110832d6156c4a267eac29c13208400d2a37173
sameerlike141 <sameerlike141@gmail.com>
2022-03-15 14:16:48 +0500

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Devops Pipline";
+        return "Hello World";
     }
 
 }
